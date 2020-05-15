ac727442f44565ab751c28edde182cf9c3bb7bf4
Sanish <skumar1@tila.com>
2020-05-16 01:48:15 +0530

    :lipstick: maniest update and full screen height

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Subtitle = styled.h2`
 `;
 export default () => {
   return (
-    <div>
+    <div className="h-screen">
       <Header />
       <div className="flex flex-col items-center justify-center mt-24 text-center">
         <Title className="px-4 text-2xl">
