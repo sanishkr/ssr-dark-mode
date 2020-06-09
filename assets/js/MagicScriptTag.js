4b6199e9c51a91c3322c0d3b49a183131f48194f
Sanish <skumar1@tila.com>
2020-06-10 00:45:42 +0530

    :art: loop of color variables in magicscript

--- assets/js/MagicScriptTag.js
+++ assets/js/MagicScriptTag.js
@@ -1,60 +1,48 @@
 import React from 'react';
-import { COLORS } from '../../config/colors.config';
+import {
+  COLORS,
+  COLOR_MODE_KEY,
+  INITIAL_COLOR_MODE_CSS_PROP,
+} from '../../config/colors.config';
+
+function setColorsByTheme() {
+  const colors = '🌈';
+  const colorModeKey = '🔑';
+  const colorModeCssProp = '⚡️';
+
+  const mql = window.matchMedia('(prefers-color-scheme: dark)');
+  const prefersDarkFromMQ = mql.matches;
+  const persistedPreference = localStorage.getItem(colorModeKey);
+
+  let colorMode = 'light';
+
+  const hasUsedToggle = typeof persistedPreference === 'string';
+
+  if (hasUsedToggle) {
+    colorMode = persistedPreference;
+  } else {
+    colorMode = prefersDarkFromMQ ? 'dark' : 'light';
+  }
+
+  let root = document.documentElement;
+
+  root.style.setProperty(colorModeCssProp, colorMode);
+
+  Object.entries(colors).forEach(([name, colorByTheme]) => {
+    const cssVarName = `--color-${name}`;
+    root.style.setProperty(cssVarName, colorByTheme[colorMode]);
+  });
+}
 
 const MagicScriptTag = () => {
-  let codeToRunOnClient = `
-(function() {
-  function getInitialColorMode() {
-  const persistedColorPreference =
-    window.localStorage.getItem('color-mode');
-  const hasPersistedPreference =
-    typeof persistedColorPreference === 'string';
-  // If the user has explicitly chosen light or dark,
-  // let's use it. Otherwise, this value will be null.
-  if (hasPersistedPreference) {
-    return persistedColorPreference;
-  }
-  // If they haven't been explicit, let's check the media
-  // query
-  const mql = window.matchMedia('(prefers-color-scheme: dark)');
-  const hasMediaQueryPreference = typeof mql.matches === 'boolean';
-  if (hasMediaQueryPreference) {
-    return mql.matches ? 'dark' : 'light';
-  }
-  // If they are using a browser/OS that doesn't support
-  // color themes, let's default to 'light'.
-  return 'light';
-}
- const colorMode = getInitialColorMode();
-  const root = document.documentElement;
-  root.style.setProperty(
-    '--color-text',
-    colorMode === 'light'
-      ? '${COLORS.text.light}'
-      : '${COLORS.text.dark}'
-  );
-  root.style.setProperty(
-    '--color-background',
-    colorMode === 'light'
-      ? '${COLORS.background.light}'
-      : '${COLORS.background.dark}'
-  );
-  root.style.setProperty(
-    '--color-primary',
-    colorMode === 'light'
-      ? '${COLORS.primary.light}'
-      : '${COLORS.primary.dark}'
-  );
-  root.style.setProperty(
-    '--color-secondary',
-    colorMode === 'light'
-      ? '${COLORS.secondary.light}'
-      : '${COLORS.secondary.dark}'
-  );
-  root.style.setProperty('--initial-color-mode', colorMode);
-})()`;
-  // eslint-disable-next-line react/no-danger
-  return <script dangerouslySetInnerHTML={{ __html: codeToRunOnClient }} />;
-};
+  const boundFn = String(setColorsByTheme)
+    .replace("'🌈'", JSON.stringify(COLORS))
+    .replace('🔑', COLOR_MODE_KEY)
+    .replace('⚡️', INITIAL_COLOR_MODE_CSS_PROP);
 
+  let calledFunction = `(${boundFn})()`;
+
+  // eslint-disable-next-line react/no-danger
+  return <script dangerouslySetInnerHTML={{ __html: calledFunction }} />;
+};
 export { MagicScriptTag };
